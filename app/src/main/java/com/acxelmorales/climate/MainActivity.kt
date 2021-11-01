f8e0785174abb23a03d089d64e79220b7d885947
Acxel Morales <acxelmorales97@gmail.com>
2021-11-01 10:16:40 -0600

    Init project

---
+++ app/src/main/java/com/acxelmorales/climate/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.acxelmorales.climate
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
